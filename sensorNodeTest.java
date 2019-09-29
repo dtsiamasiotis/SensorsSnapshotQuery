ef62b6b6580e43ac2dfa3e9dfe3f54d2aad9ac17
dtsiamasiotis <dtsiamasiotis@gmail.com>
2019-09-29 22:58:21 +0300

    initial commit

---
+++ sensorNodeTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+public class sensorNodeTest {
+
+    @Test
+    public void testJunit(){
+        SensorNode komvos = new SensorNode();
+        System.out.println("testing");
+    }
+}
